eaadd0b3e8e997c1d2aa2510244e1401a5e93ca7
yvonnehuynh79 <yvonnehuynh888@gmail.com>
2023-04-24 19:03:38 -0700

    first commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine($"Enter a number in fahrenheit that you'd like to convert to celsius");
+            var userFahrenheit = double.Parse(Console.ReadLine());
+            var celsius = TempConverter.FahrenheitToCelsius(userFahrenheit);
+            Console.WriteLine($" Temparature in celcious would be: {celsius} degrees");
+            Console.WriteLine();
+
+            
+            Console.WriteLine($"Enter a number in celsius that you'd like to conert to fahrenheit");
+            var userCelsius = double.Parse(Console.ReadLine());
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(userCelsius);
+            Console.WriteLine($"Temperature in fahrenheit is {fahrenheit} degrees");
+            Console.WriteLine();
+
+            
+            
+
+            
+            
         }
+
+        
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		
+		
+	    public static double FahrenheitToCelsius(double fahenheit)
+		{
+			return (fahenheit - 32) / 1.8;
+            
+
+		}
+
+		
+		public static double CelsiusToFahrenheit(double celsius)
+	    {
+			var result = celsius * 9 / 5 + 32;
+			return result;
+
+
+        }
+
+     }
+}
+
